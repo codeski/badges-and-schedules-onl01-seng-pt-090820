783191949105bbd01f870aa8d5ec99b4e6304ebf
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-11 22:42:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -37,3 +37,11 @@ end
   #end
 #end
 
+def printer(attendees)
+   assign_rooms(attendees).each do |name|
+    puts assign_rooms(name)
+  end
+  batch_badge_creator(attendees).each do |name|
+    puts batch_badge_creator(name)
+  end
+end
